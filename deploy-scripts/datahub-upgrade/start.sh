656bfb8070a980c6c91fee3c13584b346d838b4a
Khandelwal, Kartikey <kakhande@visa.com>
2023-08-22 12:15:06 +0530

    TLVDCLG-133, TLVDCLG-302: Migrate Dockerized Datahub on Dev to direct setup (#109)

--- deploy-scripts/datahub-upgrade/start.sh
+++ deploy-scripts/datahub-upgrade/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+TRUSTSTORE_FILE=""
+if [[ ! -z ${SSL_TRUSTSTORE_FILE:-} ]]; then
+  TRUSTSTORE_FILE="-Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE_FILE"
+fi
+
+TRUSTSTORE_PASSWORD=""
+if [[ ! -z ${SSL_TRUSTSTORE_PASSWORD:-} ]]; then
+  TRUSTSTORE_PASSWORD="-Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASSWORD"
+fi
+
 # Export all Environment Variables
 set -o allexport
 if [ -f ../../env/datahub-upgrade.env ] ; then
@@ -7,7 +17,11 @@ if [ -f ../../env/datahub-upgrade.env ] ; then
 else
   source env/datahub-upgrade.env
 fi
-[ -f ../../env/datahub-upgrade-creds.env ] && source ../../env/datahub-upgrade.env
+[ -f ../../env/datahub-upgrade-creds.env ] && source ../../env/datahub-upgrade-creds.env
 set +o allexport
 
-java -jar datahub-upgrade.jar
+export UPGRADE_INTERNAL_JAVA_OPTS="${TRUSTSTORE_FILE:-} ${TRUSTSTORE_PASSWORD:-}"
+
+exec java $UPGRADE_INTERNAL_JAVA_OPTS \
+    -jar datahub-upgrade.jar \
+    -u SystemUpdate
